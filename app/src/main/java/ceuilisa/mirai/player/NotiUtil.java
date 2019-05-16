71652337974380d822ca0964ba0a7252caec58b6
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2019-05-16 18:12:35 +0800

    initial

---
+++ app/src/main/java/ceuilisa/mirai/player/NotiUtil.java
@@ -0,0 +1,46 @@
+package ceuilisa.mirai.player;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import ceuilisa.mirai.R;
+import ceuilisa.mirai.activities.Emilia;
+
+public class NotiUtil {
+
+
+    public static void showNotification(Context context){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "12580")
+                .setSmallIcon(R.mipmap.ic_logo)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        createNotificationChannel(context);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(12581, builder.build());
+    }
+
+    private static void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channelName";
+            String description = "channelDescription";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("12580", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = Emilia.getContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
